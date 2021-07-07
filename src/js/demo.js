93f654aba2837db78a57f9b60ce8141bbd7de84d
nfvind <nicklas@davik.dk>
2021-07-07 11:04:24 +0200

    hjh

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,4 +1,4 @@
 const demo = () => 'Webpack Boilerplate v5.7.2 - SASS/PostCSS, ES6/7, browser sync, source code listing and more.';
-
+test
 // eslint-disable-next-line no-console
 console.log(demo());
